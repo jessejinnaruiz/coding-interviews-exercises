a1e198f1487d05ded121d4ebd370074ff0546aaa
Jesse Ruiz <jessejinnaruiz@gmail.com>
2024-01-05 10:42:34 -0600

    added hackerrank file

---
+++ hr-activatefountain.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'fountainActivation' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY locations as parameter.
+#
+
+def fountainActivation(locations):
+    # Write your code here
+    
