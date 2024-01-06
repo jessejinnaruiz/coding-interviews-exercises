05ee6d392f74394659541fb584d666e33b75a9fc
Jesse Ruiz <jessejinnaruiz@gmail.com>
2024-01-06 10:52:17 -0600

    adding even subarray hackerrank

---
+++ hr-evensubarray.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'evenSubarray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY numbers
+#  2. INTEGER k
+#
+
+def evenSubarray(numbers, k):
+    # Write your code here
+    
+
+if __name__ == '__main__':
