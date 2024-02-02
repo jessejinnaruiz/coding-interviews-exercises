3b5b3c4b7d1e6375ee2dbedc9088c2d233791a6c
Jesse Ruiz <jessejinnaruiz@gmail.com>
2024-02-02 11:43:11 -0600

    added remove dups from sorted array

---
+++ leet_code/26_removeduplicatesfromsortedarray.py
@@ -0,0 +1,13 @@
+nums_test_1 = [1,1,2]
+
+def removeDuplicates(nums) -> int:
+    for i, v in enumerate(nums):
+        print(nums[i])
+        print(nums[i+1])
+        if nums[i] == nums[i+1]:
+            nums.remove(nums[i+1])
+        else:
+            continue
+    return nums
+
+removeDuplicates(nums_test_1)
